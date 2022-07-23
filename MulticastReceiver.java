a3393430d62acbef51b48ecac1c68cb0c798d0f7
t-d-o-g <trc229@gmail.com>
2022-07-23 15:02:06 -0400

    Add bye exit condition

--- MulticastReceiver.java
+++ MulticastReceiver.java
@@ -7,6 +7,7 @@ public class MulticastReceiver implements Runnable {
 	private InetAddress group;
 	private int port;
 	private String msg;
+	private boolean quit = false;
 
 	public MulticastReceiver(InetAddress group, int port, String msg) {
 		this.group = group;
@@ -16,21 +17,31 @@ public class MulticastReceiver implements Runnable {
 
 	public void run() {
 		try {
-			MulticastSocket s = new MulticastSocket(port);
-			s.joinGroup(group);
+			MulticastSocket socket = new MulticastSocket(port);
+			socket.joinGroup(group);
+			
+			System.out.println("MulticastReceiver listening for messages. When you are done say bye.\n");
 
-			while (true) {
+			while (!msg.equals("bye")) {
 				byte[] data = new byte[MAX_LENGTH];
 				DatagramPacket packet = new DatagramPacket(data, data.length, group, port);
-				s.receive(packet);
+				socket.receive(packet);
 				msg = new String(packet.getData()).trim();
+
+				System.out.println("Thread: " + Thread.currentThread().getName() + "\n");
 				System.out.println("Message: " + msg);
 				System.out.println("Multicast group IP: " + group);
 				System.out.println("Mutlicast group port: " + port);
-				System.out.println("Message length: " + msg.length());
+				System.out.println("Message length: " + msg.length() + "\n");
 		  }
+			socket.leaveGroup(group);
+			socket.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void quit() {
+		quit = true;
 	}
 }
--- MulticastSender.java
+++ MulticastSender.java
@@ -1,31 +1,47 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 
 public class MulticastSender {
 	public static void main(String[] args) {
-		MulticastSocket s;
+		MulticastSocket socket;
 		InetAddress group;
 		String groupIP = "239.1.2.3";
 		int port = 3456;
 		String msg;
+		int msgCount = 1;
 
 		try {
 			if (args.length != 1)
 				msg = "Hello! Multicasting.";
 			else
 				msg = args[0];
-				
+
 			group = InetAddress.getByName(groupIP);
 
 			DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), group, port);
 			Thread recv = new Thread(new MulticastReceiver(group, port, msg));
 			recv.start();
 			
-			s = new MulticastSocket(port);
-			s.setTimeToLive(32);
-			s.send(packet);
-			s.close();
+			socket = new MulticastSocket(port);
+			socket.setTimeToLive(32);
+			Scanner scan = new Scanner(System.in);
+
+			while (!msg.equals("bye")) {
+				System.out.println("Message: " + msgCount);
+
+				socket.send(packet);
+				msg = "";
+				msg += scan.nextLine();
+				msgCount += 1;
+				recv.interrupt();
+				packet = new DatagramPacket(msg.getBytes(), msg.length(), group, port);
+			}
+			System.out.println("Message: " + msgCount);
+			scan.close();
+			packet = new DatagramPacket(msg.getBytes(), msg.length(), group, port);
+			socket.send(packet);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
